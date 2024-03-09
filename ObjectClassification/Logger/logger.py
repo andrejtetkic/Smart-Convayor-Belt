2a3050b58dc5c481072a50328abe5b426083f8c2
Andrej Tetkic <andrej.tetkic@gmail.com>
2024-03-09 23:06:02 +0100

    Add Clear Logs

--- ObjectClassification/Logger/logger.py
+++ ObjectClassification/Logger/logger.py
@@ -43,3 +43,17 @@ class PredictionLogger:
 
                 data[str(prediction)] += 1
                 json.dump(data, file, indent=2)
+
+    @staticmethod
+    def clearEverything():
+        with open(COUNT_FILE, 'w') as file:
+            data = {
+                "0": 0,
+                "1": 0,
+                "2": 0,
+            }
+
+            json.dump(data, file, indent=2)
+
+        with open(LOG_FILE, 'w') as file:
+            file.write('')
