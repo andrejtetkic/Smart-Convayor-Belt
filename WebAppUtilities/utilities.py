74144563e7c66dd7fd8bec8a9fdf08f9cd466ade
Andrej Tetkic <105948041+andrejtetkic@users.noreply.github.com>
2024-03-21 15:13:30 +0100

    added isOverlap function

--- WebAppUtilities/utilities.py
+++ WebAppUtilities/utilities.py
@@ -1,5 +1,16 @@
 def isOverlap(v1, v2):
-    return 
+    """
+    Check if two ranges overlap.
+
+    Args:
+        x (tuple): First range (start, stop).
+        y (tuple): Second range (start, stop).
+
+    Returns:
+        bool: True if the ranges overlap, False otherwise.
+    """
+    return not (v2[1] < v1[0] or v2[1] < v2[0])
+
 def checkCompability(shape_segments, color_ranges):
     for i in range(len(shape_segments)):
         for j in range(len(shape_segments)):
